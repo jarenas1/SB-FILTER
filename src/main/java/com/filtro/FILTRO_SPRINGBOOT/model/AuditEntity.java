b36275137ce6c4126db85bd40455f887055d5d2c
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-10-07 15:05:43 -0500

    create AuditEntity

--- src/main/java/com/filtro/FILTRO_SPRINGBOOT/model/AuditEntity.java
+++ src/main/java/com/filtro/FILTRO_SPRINGBOOT/model/AuditEntity.java
@@ -1,4 +1,48 @@
 package com.filtro.FILTRO_SPRINGBOOT.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+
+import java.time.LocalDateTime;
+
+@Embeddable
 public class AuditEntity {
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    //IMPLEMENTING THE CICLES OF LIVE TO CRETE THE AUDIT
+
+    @PrePersist
+    public void prePersist(){
+        System.out.println("prePersist");
+        this.createdAt = LocalDateTime.now(); //Asign the local date to the atribute
+    }
+
+    @PreUpdate
+    public void preUpdate(){
+        System.out.println("preUpdate");
+        this.updatedAt = LocalDateTime.now(); 
+
 }
